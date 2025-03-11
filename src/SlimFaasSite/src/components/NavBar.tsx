4fdcaafb3d729f9393104f1458afcb638e72d0e5
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-03-11 07:17:30 +0100

    test
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

--- src/SlimFaasSite/src/components/NavBar.tsx
+++ src/SlimFaasSite/src/components/NavBar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC = () => {
         <nav className="layout__navbar">
             <div className="navbar__left">
                 {/* Logo */}
-                <Link href="/SlimFaas">
+                <Link href="/">
                     <Image className="navbar__logo" src="/SlimFaas/slimfaas-white.svg"  alt="Logo" loading="lazy" width={64} height={64}   decoding="async"
                            data-nimg="1"  />
                 </Link>
