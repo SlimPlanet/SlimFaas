1d76de7ce8bcf02c8e92749f8d663dae47d580ae
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-03-23 18:06:15 +0100

    flx
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

--- src/SlimFaas/HostPort.cs
+++ src/SlimFaas/HostPort.cs
@@ -4,6 +4,7 @@ public class HostPort
 {
     public static bool IsSamePort(int? requestPort, int[] ports)
     {
+        Console.WriteLine("IsSamePort");
         if (requestPort == null)
         {
             return true;
@@ -11,6 +12,7 @@ public class HostPort
 
         foreach (int port in ports)
         {
+            Console.WriteLine($" port {port} == requestPort {requestPort}");
             if (port == requestPort)
             {
                 return true;
