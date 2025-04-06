11c25ff29d582be12e21c150c34254151dbbcfbf
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-04-06 20:00:35 +0200

    feature(site): add website (#93) (release)
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

---
+++ src/SlimFaasSite/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/reboot.css';
+import 'highlight.js/styles/github.css';
+import '../styles/main.scss';
+import '../styles/index.scss';
+
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+    return<>
+        <Head>
+            <link rel="icon"  type="image/svg+xml" href="/slimfaas.svg" />
+            <title>SlimFaas : The slimmest and simplest Function As A Service</title>
+        </Head>
+        <Component {...pageProps} />
+    </>;
+}
---
+++ src/SlimFaasSite/src/pages/events.tsx
@@ -0,0 +1,33 @@
+﻿import React from 'react';
+import Layout from '../components/Layout';
+import { GetStaticProps } from 'next';
+import { fetchMarkdownFile, MarkdownData, MarkdownMetadata } from '../lib/github';
+import { renderMarkdownWithHighlight } from '@/lib/markdown';
+
+export interface DocPageProps {
+    contentHtml: string;
+    metadata: MarkdownMetadata;
+}
+
+
+export const getStaticProps: GetStaticProps<DocPageProps> = async () => {
+    const markdownFile: MarkdownData = await fetchMarkdownFile(`documentation/events.md`);
+    const contentHtml = await renderMarkdownWithHighlight(markdownFile.contentHtml);
+
+    return {
+        props: {
+            contentHtml: contentHtml,
+            metadata: markdownFile.metadata || {},
+        },
+    };
+};
+
+const Events =  ({ contentHtml }: DocPageProps) => (
+    <Layout>
+        <div>
+            <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        </div>
+    </Layout>
+);
+
+export default Events;
