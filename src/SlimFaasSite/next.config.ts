5cc5b42f474c802ae4abf27354e445637614f5e6
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-04-07 16:50:31 +0200

    fix(website): test to set correct path (release)
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

--- src/SlimFaasSite/next.config.ts
+++ src/SlimFaasSite/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.NEXT_PUBLIC_DEPLOY_ENV === 'GH_PAGES';
+
 const nextConfig = {
     output: 'export',
+    basePath: '/SlimFaas',
+    assetPrefix: isGithubPages ? '/SlimFaas/' : '',
     images: {
         unoptimized: true,
     },
