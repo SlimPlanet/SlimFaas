042d5771e5d9300407617384c6ac41351748840c
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-04-08 17:16:14 +0200

    fix(website): Update next.config.ts
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

--- src/SlimFaasSite/next.config.ts
+++ src/SlimFaasSite/next.config.ts
@@ -4,7 +4,7 @@ const isGithubPages = process.env.NEXT_PUBLIC_DEPLOY_ENV === 'GH_PAGES';
 
 const nextConfig = {
     output: 'export',
-    basePath: isGithubPages ? '/SlimFaas' : '',
+    basePath: isGithubPages ? '' : '',
     assetPrefix: '' ,
     images: {
         unoptimized: true,
