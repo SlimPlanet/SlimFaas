d8e7f9142c406da8d2494c0360d38c4bbe619afa
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-05-27 07:44:25 +0200

    feature(job): add images whitelist pattern (#111) (release)
    * feature(slimfaas): job image pattern
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * doc: enhance job documentation
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * fix(slimfaas): reduce job name lenght
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * feat(slimfaas): job dynamic dependOn
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * doc(slimfaas): update job with DependsOn
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * refactor(slimfaas): add TinyGuid Tests
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * refactor(slimfaas): add regex timeout
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * refactor(slimfaas): sonar feedback
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    * fix(slimfaas): tiny int characters
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>
    
    ---------
    
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

---
+++ tests/SlimFaas.Tests/TinyGuidTests.cs
@@ -0,0 +1,19 @@
+﻿using SlimFaas.Kubernetes;
+
+namespace SlimFaas.Tests;
+
+public class TinyGuidTests
+{
+    [Fact]
+    public void TinyGuidShould()
+    {
+        var guid5 = TinyGuid.NewTinyGuid(5);
+        Assert.Equal(5, guid5.Length);
+
+        var guid3 = TinyGuid.NewTinyGuid(3);
+        Assert.Equal(3, guid3.Length);
+
+        var guid10 = TinyGuid.NewTinyGuid(10);
+        Assert.Equal(10, guid10.Length);
+    }
+}
