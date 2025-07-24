1ec961e25b24805ad9953454d78db206cac538a8
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-07-24 16:48:28 +0200

    youhou
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

--- src/SlimData/Program.cs
+++ src/SlimData/Program.cs
@@ -4,7 +4,7 @@ switch (args.LongLength)
 {
     case 0:
     case 1:
-        Console.WriteLine("Port number and protocol are not specified");
+        Console.WriteLine("Port number and protocol are not specified !");
         break;
     case 2:
         await Starter.StartNode(args[0], args[1]);
