18931a9cbe5fcd5c19627a2313b3d6f766a18252
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-08-18 15:12:42 +0200

    google analytics body (release)

--- src/SlimFaasSite/src/components/Layout.tsx
+++ src/SlimFaasSite/src/components/Layout.tsx
@@ -8,6 +8,10 @@ type LayoutProps = {
 
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PKM3T5QT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div className="layout">
        <NavBar />
         <main className="layout__content">{children}</main>
