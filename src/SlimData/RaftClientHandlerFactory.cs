b86d2afe25480abd622d140fba3c50c3bfe6f5f7
Guillaume Chervet <guillaume.chervet@gmail.com>
2025-10-25 17:46:45 +0200

    test
    Signed-off-by: Guillaume Chervet <guillaume.chervet@gmail.com>

--- src/SlimData/RaftClientHandlerFactory.cs
+++ src/SlimData/RaftClientHandlerFactory.cs
@@ -15,7 +15,23 @@ internal sealed class RaftClientHandlerFactory : IHttpMessageHandlerFactory
             throw new Exception("SLIMDATA_SOCKETS_HTTP_HANDLER_TIMEOUT is not an integer");
         }
         Console.WriteLine($"RaftClientHandlerFactory.CreateHandler({name}) with electionTimeout {electionTimeout}");
-        var handler = new SocketsHttpHandler { ConnectTimeout = TimeSpan.FromMilliseconds(electionTimeout) };
+        var handler = new SocketsHttpHandler
+        {
+            // Etablissement TCP+TLS : vise 2s en prod K8s/mesh
+            ConnectTimeout = TimeSpan.FromMilliseconds(electionTimeout),
+
+            // Evite les connexions zombifiées derrière un LB
+            PooledConnectionLifetime = TimeSpan.FromMinutes(2),
+            PooledConnectionIdleTimeout = TimeSpan.FromSeconds(30),
+
+            // HTTP/2: garde la connexion en vie (si HTTP/2 activé)
+            KeepAlivePingDelay = TimeSpan.FromSeconds(15),
+            KeepAlivePingTimeout = TimeSpan.FromSeconds(20),
+            EnableMultipleHttp2Connections = true,
+
+            UseProxy = false
+            
+        };
         handler.SslOptions.RemoteCertificateValidationCallback = AllowCertificate;
         handler.UseProxy = false;
         return handler;
