8c5c8c197f44a8658e38d80ec8dc2d1630ada825
guillaume-chervet <guillaume-chervet@users.noreply.github.com>
2026-04-28 08:45:25 +0000

    deploy: 547da85e0d0926f05ae1d2c8031b8438a73cdb43

---
+++ _next/static/bllyiFI6KBjKjsA2sYuu7/_buildManifest.js
@@ -0,0 +1,70 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0~5.8fsl~~o2d.js"
+  ],
+  "/_error": [
+    "static/chunks/15kq94_jq7lii.js"
+  ],
+  "/autoscaling": [
+    "static/chunks/074~w4yx2jmjr.js"
+  ],
+  "/data-files": [
+    "static/chunks/0g8ue-px6jet9.js"
+  ],
+  "/data-sets": [
+    "static/chunks/0_y9qc9pzyy3l.js"
+  ],
+  "/events": [
+    "static/chunks/12csk6rw9ksq..js"
+  ],
+  "/functions": [
+    "static/chunks/149.c33q2b20j.js"
+  ],
+  "/get-started": [
+    "static/chunks/01ufph8lyqptu.js"
+  ],
+  "/how-it-works": [
+    "static/chunks/0lkm7120gju30.js"
+  ],
+  "/jobs": [
+    "static/chunks/0tarki-xroqt..js"
+  ],
+  "/kafka": [
+    "static/chunks/0f7~my40s5vu-.js"
+  ],
+  "/mcp": [
+    "static/chunks/0dmoy8uj_.vm9.js"
+  ],
+  "/opentelemetry": [
+    "static/chunks/0xbj2.bqu5n2w.js"
+  ],
+  "/planet-saver": [
+    "static/chunks/0~aoqbgi02~~t.js"
+  ],
+  "/user-interface": [
+    "static/chunks/0y_9.ujn6ojj-.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/autoscaling",
+    "/data-files",
+    "/data-sets",
+    "/events",
+    "/functions",
+    "/get-started",
+    "/how-it-works",
+    "/jobs",
+    "/kafka",
+    "/mcp",
+    "/opentelemetry",
+    "/planet-saver",
+    "/user-interface"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/0f7~my40s5vu-.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/kafka", [
+  "static/chunks/082aksc~1rr7g.js",
+  "static/chunks/0h8bdnevgyteh.js",
+  "static/chunks/0._i987p-x-nz.js",
+  "static/chunks/0vxfz0qk_54vv.js",
+  "static/chunks/0ght619bqydu9.js",
+  "static/chunks/turbopack-09mjm4iw75atb.js"
+])
