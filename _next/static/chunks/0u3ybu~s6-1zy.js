453f9b8722424da3b332316518087a13c3b808c8
guillaume-chervet <guillaume-chervet@users.noreply.github.com>
2026-05-05 21:26:55 +0000

    deploy: b06371a3972cdda9413ad9c81dcfd79534d4ca3a

---
+++ _next/static/chunks/0u3ybu~s6-1zy.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/user-interface", [
+  "static/chunks/185a1qauz64.e.js",
+  "static/chunks/0tzvdfi90kujd.js",
+  "static/chunks/0._i987p-x-nz.js",
+  "static/chunks/0vxfz0qk_54vv.js",
+  "static/chunks/0ght619bqydu9.js",
+  "static/chunks/turbopack-0dfg3jpka44rt.js"
+])
---
+++ _next/static/x4P05tsJy3mdfxIk10UTq/_buildManifest.js
@@ -0,0 +1,74 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0e5nlw9ewstzj.js"
+  ],
+  "/_error": [
+    "static/chunks/15kq94_jq7lii.js"
+  ],
+  "/autoscaling": [
+    "static/chunks/12f839gtl-6y3.js"
+  ],
+  "/clients": [
+    "static/chunks/0raw5.u2fw8x8.js"
+  ],
+  "/data-files": [
+    "static/chunks/17c1qttn97-b2.js"
+  ],
+  "/data-sets": [
+    "static/chunks/06g0vt4v3vy9e.js"
+  ],
+  "/events": [
+    "static/chunks/0~j~2cq_95qun.js"
+  ],
+  "/functions": [
+    "static/chunks/08ge2zlayps7x.js"
+  ],
+  "/get-started": [
+    "static/chunks/0~q8o~0xz8mng.js"
+  ],
+  "/how-it-works": [
+    "static/chunks/0.7ki8xwo65~..js"
+  ],
+  "/jobs": [
+    "static/chunks/0ebgqs1637v40.js"
+  ],
+  "/kafka": [
+    "static/chunks/0603fkva9e-8k.js"
+  ],
+  "/mcp": [
+    "static/chunks/0_nvisw6o8.9m.js"
+  ],
+  "/opentelemetry": [
+    "static/chunks/17h.53-eibz_t.js"
+  ],
+  "/planet-saver": [
+    "static/chunks/0g7vd5dtfqggo.js"
+  ],
+  "/user-interface": [
+    "static/chunks/0u3ybu~s6-1zy.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/autoscaling",
+    "/clients",
+    "/data-files",
+    "/data-sets",
+    "/events",
+    "/functions",
+    "/get-started",
+    "/how-it-works",
+    "/jobs",
+    "/kafka",
+    "/mcp",
+    "/opentelemetry",
+    "/planet-saver",
+    "/user-interface"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
